8b167e9d98d8e13be45df4e39ad4cb829df0c0a9
Kitgun1 <kitgunplay@gmail.com>
2025-03-18 21:47:05 +1000

    feat: storage add save and load objects

--- Assets/Assets/Scripts/StorageGame/Storage.cs
+++ Assets/Assets/Scripts/StorageGame/Storage.cs
@@ -12,7 +12,9 @@ namespace AuroraWorld.StorageGame
             _storage = new WindowsStorage();
             // TODO: storage for server, yandex game server
 #endif
-            
         }
+
+        public void Save<T>(string tag, T obj) => _storage.Save(tag, obj);
+        public T Load<T>(string tag, T defaultObj = default) => _storage.Load(tag, defaultObj);
     }
 }
