dd41da12ce0ce36ba8f2b5bc3b348b86115ae1cc
Kitgun1 <kitgunplay@gmail.com>
2025-03-29 19:31:42 +1000

    feat: register factories

--- Assets/Assets/Scripts/LobbyContext.cs
+++ Assets/Assets/Scripts/LobbyContext.cs
@@ -10,6 +10,21 @@ namespace AuroraWorld
         public void Init(DIContainer projectContainer)
         {
             _lobbyContainer = new DIContainer(projectContainer);
+
+            _lobbyContainer.RegisterSingleton(UI_CAMERA_CONFIG, UICameraConfigFactory);
+            _lobbyContainer.RegisterSingleton(UI_CAMERA, UICameraFactory);
         }
+
+        #region Factories
+
+        private const string UI_CAMERA_CONFIG = nameof(UI_CAMERA_CONFIG);
+        private const string UI_CAMERA = nameof(UI_CAMERA);
+        
+        private static Camera UICameraFactory(DIContainer c) => new(c.Resolve<CameraConfiguration>(UI_CAMERA_CONFIG));
+
+        private static CameraConfiguration UICameraConfigFactory(DIContainer _)
+            => new(null, (0, 10), CameraClearFlags.Nothing, true, 1 << LayerMask.NameToLayer("UI"));
+
+        #endregion
     }
 }
