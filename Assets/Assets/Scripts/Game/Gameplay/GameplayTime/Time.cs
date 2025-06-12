c804b96b2af24063eb6e025fa7c68b9f620c10d7
Kitgun1 <kigunplay@gmail.com>
2025-06-12 20:56:42 +1000

    feat: time initial

---
+++ Assets/Assets/Scripts/Game/Gameplay/GameplayTime/Time.cs
@@ -0,0 +1,12 @@
+namespace AuroraWorld.Gameplay.GameplayTime
+{
+    public class Time
+    {
+        public int Tick;
+
+        public Time(int tick)
+        {
+            Tick = tick;
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/Game/Gameplay/GameplayTime/TimeProxy.cs
@@ -0,0 +1,33 @@
+using System;
+using R3;
+using UnityEngine;
+
+namespace AuroraWorld.Gameplay.GameplayTime
+{
+    public class TimeProxy
+    {
+        public ReactiveProperty<int> Tick;
+
+        public Time Origin;
+
+        private IDisposable _tickObservable;
+        
+        public TimeProxy(Time origin)
+        {
+            Origin = origin;
+
+            Tick = new ReactiveProperty<int>();
+
+            _tickObservable = Observable.Interval(TimeSpan.FromSeconds(0.1)).Subscribe(tick =>
+            {
+                Debug.Log($"tick: {tick}");
+            }).AddTo(CreateRootObject());
+        }
+
+        private MonoBehaviour CreateRootObject()
+        {
+            var rootObject = new GameObject("[TIME]");
+            return rootObject.AddComponent<MonoBehaviour>();
+        }
+    }
+}
