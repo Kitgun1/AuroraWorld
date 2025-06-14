e9a026be7e1616588ca10201ca099c3dae02b9d5
Kitgun1 <kigunplay@gmail.com>
2025-06-14 18:41:44 +1000

    feat: added time params [Hour, Day, Quart, Year], updated time by interval

--- Assets/Assets/Scripts/Game/Gameplay/GameplayTime/Time.cs
+++ Assets/Assets/Scripts/Game/Gameplay/GameplayTime/Time.cs
@@ -1,12 +1,14 @@
+using System;
+
 namespace AuroraWorld.Gameplay.GameplayTime
 {
+    [Serializable]
     public class Time
     {
         public int Tick;
-
-        public Time(int tick)
-        {
-            Tick = tick;
-        }
+        public int Hour;
+        public int Day;
+        public int Quart;
+        public int Year;
     }
 }
--- Assets/Assets/Scripts/Game/Gameplay/GameplayTime/TimeProxy.cs
+++ Assets/Assets/Scripts/Game/Gameplay/GameplayTime/TimeProxy.cs
@@ -1,4 +1,5 @@
 using System;
+using DI;
 using R3;
 using UnityEngine;
 
@@ -6,28 +7,60 @@ namespace AuroraWorld.Gameplay.GameplayTime
 {
     public class TimeProxy
     {
-        public ReactiveProperty<int> Tick;
+        public ReactiveProperty<int> Tick { get; }
+        public ReactiveProperty<int> Hour { get; }
+        public ReactiveProperty<int> Day { get; }
+        public ReactiveProperty<int> Quart { get; }
+        public ReactiveProperty<int> Year { get; }
 
-        public Time Origin;
+        public readonly Time Origin;
 
-        private IDisposable _tickObservable;
-        
-        public TimeProxy(Time origin)
+        public TimeProxy(DIContainer container, Time origin)
         {
+            container.RegisterInstance(this);
             Origin = origin;
 
-            Tick = new ReactiveProperty<int>();
+            Tick = new ReactiveProperty<int>(Origin.Tick);
+            Hour = new ReactiveProperty<int>(Origin.Hour);
+            Day = new ReactiveProperty<int>(Origin.Day);
+            Quart = new ReactiveProperty<int>(Origin.Quart);
+            Year = new ReactiveProperty<int>(Origin.Year);
 
-            _tickObservable = Observable.Interval(TimeSpan.FromSeconds(0.1)).Subscribe(tick =>
+            Observable.Interval(TimeSpan.FromMilliseconds(500))
+                .Subscribe(_ => Tick.Value = (Tick.Value + 1) % 10)
+                .AddTo(container.Resolve<Transform>("ParentMeshTransform"));
+
+            Tick.Skip(1).Subscribe(t =>
             {
-                Debug.Log($"tick: {tick}");
-            }).AddTo(CreateRootObject());
-        }
+                Origin.Tick = t;
+                if (t % 10 == 0)
+                {
+                    Hour.Value++;
+                }
 
-        private MonoBehaviour CreateRootObject()
-        {
-            var rootObject = new GameObject("[TIME]");
-            return rootObject.GetComponent<MonoBehaviour>();
+                if (Hour.Value != 0 && Hour.Value % 24 == 0)
+                {
+                    Hour.Value = 0;
+                    Day.Value++;
+                }
+
+                if (Day.Value != 0 && Day.Value % 24 == 0)
+                {
+                    Day.Value = 0;
+                    Quart.Value++;
+                }
+
+                if (Quart.Value != 0 && Quart.Value % 4 == 0)
+                {
+                    Quart.Value = 0;
+                    Year.Value++;
+                }
+            });
+
+            Hour.Skip(1).Subscribe(h => Origin.Hour = h);
+            Day.Skip(1).Subscribe(d => Origin.Day = d);
+            Quart.Skip(1).Subscribe(q => Origin.Quart = q);
+            Year.Skip(1).Subscribe(y => Origin.Year = y);
         }
     }
 }
